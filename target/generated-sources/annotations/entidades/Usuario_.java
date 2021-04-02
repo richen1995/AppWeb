07cca9e9382022c6950e881c9dda423254d6ffc7
richen1995 <richen1995@gmail.com>
2021-04-02 18:34:46 -0500

    Probar Heroku

--- target/generated-sources/annotations/entidades/Usuario_.java
+++ target/generated-sources/annotations/entidades/Usuario_.java
@@ -7,7 +7,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-01T00:21:09")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2021-04-02T18:28:54")
 @StaticMetamodel(Usuario.class)
 public class Usuario_ { 
 
